4cdd7e84e00acaa7afa4dc5d5328c8eeee4e49e2
davidauza.engineer <davidauza01@gmail.com>
2020-05-27 15:39:55 -0500

    Implement pagination for search results

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,9 @@
 class CustomersController < ApplicationController
 
+  PAGE_SIZE = 10
+
   def index
+    @page = (params[:page] || 0).to_i
     if params[:keywords].present?
       @keywords = params[:keywords]
       customer_search_term = CustomerSearchTerm.new(@keywords)
@@ -9,6 +12,10 @@ class CustomersController < ApplicationController
         customer_search_term.where_args
       ).order(
         customer_search_term.order
+      ).offset(
+        PAGE_SIZE * @page
+      ).limit(
+        PAGE_SIZE
       )
     else
       @customers = []
