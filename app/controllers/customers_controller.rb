a527364eccc0f914cdfdfe35eb21cfab1a417623
davidauza.engineer <davidauza01@gmail.com>
2020-05-27 12:08:37 -0500

    Create basic implementation for the search feature

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,17 @@
 class CustomersController < ApplicationController
 
   def index
-    @customers = Customer.all.limit(10)
+    if params[:keywords].present?
+      @keywords = params[:keywords]
+      customer_search_term = CustomerSearchTerm.new(@keywords)
+      @customers = Customer.where(
+        customer_search_term.where_clause,
+        customer_search_term.where_args
+      ).order(
+        customer_search_term.order
+      )
+    else
+      @customers = []
+    end
   end
 end
